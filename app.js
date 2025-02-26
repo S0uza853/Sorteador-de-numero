b297ddbc32b541140d909f947cd172db03837d2d
Igor Souza <igormartins.souza1@gmail.com>
2025-02-26 14:55:49 -0300

    colocando limite em DE e ATE

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document .getElementById('ate').value);
 
+    if (de > ate) {
+        alert ('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return
+    }
+
  let sorteados = [];
  let numero;
 
@@ -38,10 +43,10 @@ function alterarStatusBotao() {
 }
 
 function reiniciar() {
-    document .getElementById('quantidade').value;
-    document .getElementById('de').value;
-    document .getElementById('ate').value;
-    document .getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
     alterarStatusBotao();
 
 }
